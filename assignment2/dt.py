d005168110d58c47e2a650182367ff96f8b40881
bae jiun, maybe <alice.maydev@gmail.com>
2018-04-18 15:42:47 +0900

    New decision tree assignment2

---
+++ assignment2/dt.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import argparse
+
+import pandas as pd
+
+from lib.metric import Gini
+from lib.tree import DecisionTree
+
+def main(train_file, test_file, result_file, metric):
+    # load data
+    train = pd.read_csv(train_file, sep='\t')
+    test = pd.read_csv(test_file, sep='\t')
+
+    # y_label, class to predict
+    y_label, *_ = list(set(train.columns) - set(test.columns))
+
+    # categorical data to numeric data (just encode)
+    df, labels = pd.concat([train, test]), dict()
+    for column in df:
+        df[column], labels[column] = pd.factorize(df[column])
+        labels[column] = labels[column].get_values()
+
+    # split x, y, train, test set
+    x_train = df.iloc[:len(train)].drop(y_label, axis=1)
+    y_train = df.iloc[:len(train)][y_label]
+    x_test = df.iloc[len(train):].drop(y_label, axis=1)
+
+    classifier = DecisionTree(Gini)
+
+    classifier.fit(x_train, y_train, 16, 0)
+
+    test[y_label] = pd.Series(map(lambda y: labels[y_label][y], classifier.predict(x_test)))
+
+    # write result
+    test.to_csv(result_file, sep='\t', index=None)
+
+if __name__ == '__main__':
+    # argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument("train", help="minimum support", type=str)
+    parser.add_argument("test", help="input file name", type=str)
+    parser.add_argument("result", help="output file name", type=str)
+    parser.add_argument("--metric", help="select metric to apply", dest='metric', choices=['gini', 'infogain', 'gain'])
+    args = parser.parse_args()
+
+    main(args.train, args.test, args.result, args.metric)
