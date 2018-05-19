bfe60bc0b4b6b09704bb09b580cb77f35af31e90
bae jiun, maybe <alice.maydev@gmail.com>
2018-05-19 20:22:22 +0900

    Update lib.clustering

--- assignment3/lib/clustering.py
+++ assignment3/lib/clustering.py
@@ -3,57 +3,48 @@ from collections import defaultdict
 import numpy as np
 
 class DBSCAN:
+    """ Simple implementation of DBSCAN clustering
+    """
     def __init__(self, data, eps, minpts):
-        self.obj = data[:, 0]
-        self.pos = data[:, 1:]
-        self.labels = [None] * len(data)
+        self.data = data
+        self.labels = [0] * len(data)
 
         self.eps = eps
         self.minpts = minpts
         self.count = 0
-        self.clusters = defaultdict(list)
-        
+        self._clusters = defaultdict(list)
+
     def __len__(self):
         return self.count
 
-    def _metric(self, left, right):
-        return np.linalg.norm(left - right)
-    
+    @property
+    def clusters(self):
+        if not self._clusters: self._dbscan()
+        return self._clusters
+
     def _grow(self, index, neighbors):
+        self.count += 1
         self.labels[index] = self.count
         for neighbor, *_ in neighbors:
             if self.labels[neighbor] == -1:
                 self.labels[neighbor] = self.count
-            elif self.labels[neighbor] == None:
+            elif not self.labels[neighbor]:
                 self.labels[neighbor] = self.count
                 sub_neighbors = self._neighbor(neighbor)
                 if len(sub_neighbors) >= self.minpts:
                     neighbors += sub_neighbors
 
     def _neighbor(self, index):
-        condition = lambda x: self._metric(self.pos[index], x) <= self.eps
-        return [(i, *pos) for i, pos in enumerate(self.pos) if condition(pos)]
-        
+        condition = lambda pos: np.linalg.norm(self.data[index] - pos) <= self.eps
+        return [(i, *pos) for i, pos in enumerate(self.data) if condition(pos)]
+
     def _dbscan(self):
-        for index, _ in enumerate(self.obj):
+        for index, _ in enumerate(self.data):
             if self.labels[index]: continue
             neighbors = self._neighbor(index)
             if len(neighbors) < self.minpts:
                 self.labels[index] = -1
             else:
                 self._grow(index, neighbors)
-                self.count += 1
-        for obj, label in zip(self.obj, self.labels):
-            self.clusters[label].append(obj)
-
-    def get(self, n=0):
-        if not self.clusters: self._dbscan()
-        if not n: return self.clusters.items()
-        if n > len(self.clusters):
-            raise Exception('Not enough clusters')
-        clusters = defaultdict(list)
-        for key, value in sorted(self.clusters.items(), key=lambda x: -len(x[1])):
-            if len(clusters) == n: break
-            if key != -1:
-                clusters[key] = value
-        return clusters.items()
+        for index, label in enumerate(self.labels):
+            self._clusters[label].append(index)
