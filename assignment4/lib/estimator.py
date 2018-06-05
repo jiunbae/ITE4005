0d5b38ed564401853b6ac887857ac1853999634d
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-05 10:15:35 +0900

    Update simple implementation of estimater

---
+++ assignment4/lib/estimator.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Estimator
+
+This is part of ITE4005 assignment#4 @ Hanyang Univ.
+Author: Bae Jiun, Maybe
+
+This module for definition of estimator
+"""
+
+from typing import List
+
+class Estimator:
+    """Base estimator
+    """
+
+    def __init__(self):
+        """Base estimator
+        """
+        pass
+
+    def fit(self, X: List, y: List) -> None:
+        """fit estimator to input X, y
+
+        @param: X               train X
+        @param: y               train y
+        """
+        pass
+
+    def predict(self, X: List[List]) -> List[float]:
+        """predict using trained estimator with input X
+
+        @param: X               test X
+        """
+        pass
---
+++ assignment4/lib/recommender.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""Decision tree
+
+This is part of ITE4005 assignment#2 @ Hanyang Univ.
+Author: Bae Jiun, Maybe
+
+This module implement decision tree
+"""
+
+from typing import Any
+
+class Recommander:
+    def __init__(self, algorithm):
+        pass
+
+    def fit(self, 
+            X: np.ndarray,
+            y: np.ndarray):
+        pass
+
+    def predict(self, X: np.ndarray) -> Any:
+        pass
