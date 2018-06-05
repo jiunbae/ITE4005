ed41b4d372d684c943a1830fe407dbd9cddbac3e
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-05 10:15:17 +0900

    Update baseline mode

--- assignment4/recommender.py
+++ assignment4/recommender.py
@@ -1,11 +1,27 @@
 #!/bin/python3
+# -*- coding: utf-8 -*-
 
+"""Recommender System
+
+This is part of ITE4005 assignment#4 @ Hanyang Univ.
+Author: Bae Jiun, Maybe
+"""
 import argparse
+from os.path import splitext
 
 import pandas as pd
 
+from .lib.recommender import Recommender
+
 def main(train_file, test_file):
-    data = pd.read_csv(train_file, sep='\t', header=None)
+    train = pd.read_csv(train_file, sep='\t', header=None)
+    test = pd.read_csv(test_file, sep='\t', header=None)
+
+    model = Recommender()
+    model.fit(train.values[:, :2], train.values[:, -1])
+
+    test['rating'] = pd.Series(model.predict(test.values))
+    test.to_csv(splitext(test_file)[0] + '.base_prediction.txt', sep='\t', index=None)
 
 if __name__ == '__main__':
     # argument parser
