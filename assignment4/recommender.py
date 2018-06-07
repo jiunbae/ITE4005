11e23cdcef628343dca32d9f2173271e1a389b98
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-07 14:37:02 +0900

    Update recommender to show process time

--- assignment4/recommender.py
+++ assignment4/recommender.py
@@ -8,21 +8,25 @@ Author: Bae Jiun, Maybe
 """
 import argparse
 from os.path import splitext
-
+from time import time
 import pandas as pd
 
-from lib.recommender import Recommander
+from lib import timer
+from lib.recommender import Recommender
 
 def main(train_file, test_file):
+    timer.begin()
     train = pd.read_csv(train_file, sep='\t', header=None)
     test = pd.read_csv(test_file, sep='\t', header=None)
 
-    model = Recommander()
+    model = Recommender()
     model.fit(train.values[:, :2], train.values[:, 2])
 
+    predicts = model.predict(test.values[:, :2])
     test = test.drop(test.columns[-1], axis=1)
-    test[test.columns[-1]] = pd.Series(model.predict(test.values))
+    test[test.columns[-1]] = pd.Series(predicts)
     test.to_csv(splitext(test_file)[0] + '.base_prediction.txt', sep='\t', index=None, header=None)
+    print ('recommender performed in', timer.end())
 
 if __name__ == '__main__':
     # argument parser
