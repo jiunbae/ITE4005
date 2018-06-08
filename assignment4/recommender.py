e315a22a465e0c6360e183c8e3940aee2cbe8878
bae jiun, maybe <alice.maydev@gmail.com>
2018-06-08 17:28:30 +0900

    Update argument parser

--- assignment4/recommender.py
+++ assignment4/recommender.py
@@ -14,12 +14,12 @@ import pandas as pd
 from lib import timer
 from lib.recommender import Recommender
 
-def main(train_file, test_file):
+def main(train_file, test_file, args):
     timer.begin()
     train = pd.read_csv(train_file, sep='\t', header=None)
     test = pd.read_csv(test_file, sep='\t', header=None)
 
-    model = Recommender()
+    model = Recommender(**args)
     model.fit(train.values[:, :2], train.values[:, 2])
 
     predicts = model.predict(test.values[:, :2])
@@ -38,5 +38,25 @@ if __name__ == '__main__':
                         help="test file path",
                         type=str)
 
+    parser.add_argument("--factor",
+                        dest="factor", help="size of factor", type=int, default=100)
+    parser.add_argument("--epoch",
+                        dest="epoch", help="epoch size", type=int, default=20)
+    parser.add_argument("--mean",
+                        dest="mean", help="initial mean value", type=float, default=.0)
+    parser.add_argument("--dev",
+                        dest="dev", help="initial derivation value", type=float, default=.1)
+    parser.add_argument("--lr",
+                        dest="lr", help="leraning rate", type=float, default=.005)
+    parser.add_argument("--reg",
+                        dest="reg", help="regression rate", type=float, default=.02)
+
     args = parser.parse_args()
-    main(args.train, args.test)
+    main(args.train, args.test, {
+        'factors': args.factor,
+        'epochs': args.epoch,
+        'mean': args.mean,
+        'derivation': args.dev,
+        'lr': args.lr,
+        'reg': args.reg,
+    })
